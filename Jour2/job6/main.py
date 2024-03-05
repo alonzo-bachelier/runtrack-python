4f9f4ea639502ba0bf80fc45bca618fbc9db7aca
alonzo-bachelier <alonzo.bachelier@laplateforme.io>
2024-03-05 14:08:28 +0100

    gg

--- Jour2/job6/main.py
+++ Jour2/job6/main.py
@@ -1,3 +1,16 @@
-number = list(range(0, 1001))
-
-print(number)
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+ 
+primes = []
+n = 2
+while len(primes) < 200:
+    if is_prime(n):
+        primes.append(n)
+    n += 1
+ 
+print(primes)
